47ba609145399ae5b96b85873d41551a7f67bffa
Hamidur Rahman <Hamidur0405@gmail.com>
2020-01-31 22:11:51 -0500

    completed priority queue

--- src/priorityQueue/PriorityQueue.java
+++ src/priorityQueue/PriorityQueue.java
@@ -8,15 +8,17 @@ public class PriorityQueue<T extends Comparable<T>> implements Queue<T>
     {
         private T data;
         private Node<T> next;
+        private Node<T> previous;
 
         private Node(T data)
         {
-            this(data, null);
+            this(data, null, null);
         }
 
-        private Node(T data, Node<T> next)
+        private Node(T data, Node<T> previous, Node<T> next)
         {
             this.data = data;
+            this.previous = previous;
             this.next = next;
         }
     }
@@ -27,34 +29,119 @@ public class PriorityQueue<T extends Comparable<T>> implements Queue<T>
     public PriorityQueue() {}
 
     @Override
-    public void add(T element) throws NullPointerException {
+    public void add(T element) throws NullPointerException
+    {
+        if(element == null) throw new NullPointerException("NULL given.");
+        if(isEmpty())
+        {
+            head = new Node<>(element);
+            size++;
+        }
+        else
+        {
+            bubbleDown(new Node<>(element));
+            size++;
+        }
+    }
 
+    private void bubbleDown(Node<T> node)
+    {
+        if(node.data.compareTo(head.data) < 0)
+        {
+            node.next = head;
+            head.previous = node;
+            head = node;
+            return;
+        }
+
+        if(size() ==  1)
+        {
+            head.next = node;
+            node.previous = head;
+            return;
+        }
+
+        Node<T> temp = head;
+
+        while (temp.next != null)
+        {
+            if(node.data.compareTo(temp.data) < 0)
+            {
+                if(temp.previous == null)
+                {
+                    node.next = temp;
+                    temp.previous = node;
+                    return;
+                }
+                else
+                {
+                    node.next = temp;
+                    node.previous = temp.previous;
+                    temp.previous.next = node;
+                    return;
+                }
+            }
+            else if(node.data.compareTo(temp.data) == 0)
+            {
+                node.next = temp.next;
+                node.previous = temp;
+                temp.next = node;
+                return;
+            }
+            temp = temp.next;
+        }
+
+        if(node.data.compareTo(temp.data) < 0)
+        {
+            node.next = temp;
+            node.previous = temp.previous;
+            temp.previous.next = node;
+        }
+        else
+        {
+            temp.next = node;
+            node.previous = temp;
+        }
     }
 
     @Override
-    public T removeMin() throws NoSuchElementException {
-        return null;
+    public T removeMin() throws NoSuchElementException
+    {
+        if(isEmpty()) throw new NoSuchElementException("Empty queue.");
+        T data = head.data;
+        if(head.next == null)
+        {
+            head = null;
+            size = 0;
+            return data;
+        }
+        else
+        {
+            head = head.next;
+            size--;
+            return data;
+        }
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size() == 0;
     }
 
     @Override
     public String toString()
     {
         StringBuilder stringBuilder = new StringBuilder();
-        while (head != null)
-        {
-            stringBuilder.append(head.data + " ->");
-            head = head.next;
-        }
+//        while (head != null)
+//        {
+//            stringBuilder.append(head.data + " -> ");
+//            head = head.next;
+//        }
         return stringBuilder.toString();
     }
 }
