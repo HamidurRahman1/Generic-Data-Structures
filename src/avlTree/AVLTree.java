3b877b81b63d1ef20c3ebd939895a345259d7f8c
Hamidur Rahman <Hamidur0405@gmail.com>
2020-07-20 10:12:01 -0400

    partial implementation

--- src/avlTree/AVLTree.java
+++ src/avlTree/AVLTree.java
@@ -67,38 +67,146 @@ public class AVLTree<T extends Comparable> implements Tree<T>
         }
     }
 
+    private Node<T> root;
+    private int size;
+
+    public AVLTree() {}
+
+    private void addRoot(T data)
+    {
+        if(root == null)
+        {
+            root = new Node<>(data);
+            size++;
+        }
+    }
+
     @Override
     public void add(T data) {
+        Node<T> parent = find(data);
+        if (parent == null)
+            addRoot(data);
+        else if (parent.data.compareTo(data) > 0)
+            addLeft(parent, data);
+        else if (parent.data.compareTo(data) < 0)
+            addRight(parent, data);
+    }
 
+    private void addLeft(Node<T> parent, T data)
+    {
+        Node<T> left = new Node<>(data);
+        left.parent = parent;
+        parent.left = left;
+        size++;
+//        reBalance(parent);
+    }
+
+    private void addRight(Node<T> parent, T data)
+    {
+        Node<T> right = new Node<>(data);
+        right.parent = parent;
+        parent.right = right;
+        size++;
+//        reBalance(parent);
     }
 
     @Override
     public boolean contains(T data) {
-        return false;
+        Node<T> node = find(data);
+        if(node == null || !node.data.equals(data)) return false;
+        return true;
     }
 
     @Override
     public void remove(T data) {
+        Node<T> node = find(data);
+        if(node == null || !node.data.equals(data)) return;
+        Node<T> parent = remove(node);
+//        reBalance(parent);
+    }
 
+    private Node<T> remove(Node<T> node)
+    {
+        if (isLeaf(node))
+        {
+            Node<T> parent = node.parent;
+            if (parent == null) root = null;
+            else parent.removeChild(node);
+            size--;
+            return parent;
+        }
+        Node<T> descendant = descendant(node);
+        promoteDescendant(node, descendant);
+        return remove(descendant);
+    }
+
+    private void promoteDescendant(Node<T> parent, Node<T> descendant)
+    {
+        parent.data = descendant.data;
+    }
+
+    private Node<T> descendant(Node<T> parent)
+    {
+        Node<T> child = parent.left;
+        if (child != null)
+        {
+            while (child.right != null) child = child.right;
+            return child;
+        }
+        child = parent.right;
+        if (child != null)
+        {
+            while (child.left != null) child = child.left;
+            return child;
+        }
+        return child;
+    }
+
+    private boolean isInternal(Node<T> node)
+    {
+        return node.children().hasNext();
+    }
+
+    private boolean isLeaf(Node<T> node)
+    {
+        return !isInternal(node);
+    }
+
+    private Node<T> find(T data)
+    {
+        if(root == null) return null;
+        return findRecursively(root, data);
+    }
+
+    private Node<T> findRecursively(Node<T> parent, T data)
+    {
+        int comparison = data.compareTo(parent.data);
+        if(comparison == 0) return parent;
+        else if(comparison < 0 && parent.left != null) return findRecursively(parent.left, data);
+        else if(comparison > 0 && parent.right != null) return findRecursively(parent.right, data);
+        return parent;
     }
 
     @Override
     public T get(T data) {
-        return null;
+        Node<T> node = find(data);
+        if(node == null || !node.data.equals(data)) return null;
+        return node.data;
     }
 
     @Override
     public void clear() {
-
+        root = null;
+        size = 0;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size() == 0;
     }
 }
---
+++ src/avlTree/Tester.java
@@ -0,0 +1,10 @@
+package avlTree;
+
+public class Tester
+{
+    public static void main(String[] args) {
+        Tree<Integer> avl = new AVLTree<>();
+        System.out.println(avl.isEmpty());
+        System.out.println(avl.isEmpty());
+    }
+}
