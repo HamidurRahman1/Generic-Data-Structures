69ed6ad157d5b5b9643c2113ec8be24e87c58495
Hamidur Rahman <Hamidur0405@gmail.com>
2020-08-13 13:03:13 -0400

    reverse itr tested

--- src/doublyLinkedList/Tester.java
+++ src/doublyLinkedList/Tester.java
@@ -6,13 +6,15 @@ public class Tester
 {
     public static void main(String[] args) {
         List<String> d = new DoublyLinkedList<>();
-        d.add(0, "1a");
-        d.add(1,  "2a");
-        d.add(2, "3a");
-        d.addLast( "4a");
-        d.addLast( "5a");
+        d.add(0, "a");
+        d.add(1,  "b");
+        d.add(2, "c");
+        d.addLast( "d");
+        d.addLast( "e");
+        d.addFirst("f");
+        d.addLast("g");
 
-        Iterator<String> iterator = ((DoublyLinkedList<String>) d).iterator();
+        Iterator<String> iterator = ((DoublyLinkedList<String>) d).reverseIterator();
         while (iterator.hasNext()){
             System.out.println(iterator.next());
         }
